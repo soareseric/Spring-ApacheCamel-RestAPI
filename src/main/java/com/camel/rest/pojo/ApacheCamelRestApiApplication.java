873c2d65b129e2e64c9bcda1fb43d29cfebf04ec
soareseric <contato.eric.sm@gmail.com>
2018-12-10 16:29:29 -0200

    Projeto criado

---
+++ src/main/java/com/camel/rest/pojo/ApacheCamelRestApiApplication.java
@@ -0,0 +1,12 @@
+package com.camel.rest.pojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheCamelRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApacheCamelRestApiApplication.class, args);
+	}
+}
